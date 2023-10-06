0c9f40bb6b9eb57284f0958a9e3b23b0ab7c16a2
ykovtun <ykovtun@stud.khpi.ucode-connect.study>
2023-10-06 18:35:12 +0300

    Refactor mx_handle_err

--- src/mx_handle_err.c
+++ src/mx_handle_err.c
@@ -5,17 +5,17 @@ void mx_handle_err(int err_type, void* param) {
         case INVALID_ARGS:
             mx_printerr("usage: ./pathfinder [filename]\n");
             break;
- 	    case INVALID_FILE:
+	case INVALID_FILE:
             mx_printerr("error: file ");
             mx_printerr((char*)param);
             mx_printerr(" does not exist\n");
             break;
- 	    case EMPTY_FILE:
+	case EMPTY_FILE:
             mx_printerr("error: file ");
             mx_printerr((char*)param);
             mx_printerr(" is empty\n");
             break;
- 	    case INVALID_FIRST_LINE:
+	case INVALID_FIRST_LINE:
             mx_printerr("error: line 1 is not valid\n");
             break;
         case INVALID_LINE:
@@ -26,10 +26,10 @@ void mx_handle_err(int err_type, void* param) {
         case INVALID_NUM_OF_ISLANDS:
             mx_printerr("error: invalid number of islands\n");
             break;
-	    case DUPLICATE_BRIDGES:
+	case DUPLICATE_BRIDGES:
             mx_printerr("error: duplicate bridges\n");
             break;
-	    case INVALID_SUM_OF_BRIDGES:
+	case INVALID_SUM_OF_BRIDGES:
             mx_printerr("error: sum of bridges lengths is too big\n");
             break;    
     }
