76a02afc33165c038fe8b94359daadf5cf23a620
ykovtun <ykovtun@stud.khpi.ucode-connect.study>
2023-10-15 18:11:17 +0300

    fix multiple definition of mx_print_path

--- src/mx_dijkstra.c
+++ src/mx_dijkstra.c
@@ -1,32 +1,5 @@
 #include "../inc/pathfinder.h"
 
-void mx_print_path(int start, int end, int distance, t_list* route, t_list* islands) {
-    mx_printstr("========================================\n");
-    mx_printstr("Path: ");
-    mx_printstr((char*)mx_list_get_by_index(islands, start));
-    mx_printstr(" -> ");
-    mx_printstr((char*)mx_list_get_by_index(islands, end));
-    mx_printchar('\n');
-    mx_printstr("Route: ");
-
-    int size = mx_list_size(route);
-    t_list* tmp = route;
-    for(int i = 0; i < size; i++) {
-        if(i == size - 1) {
-            mx_printstr((char*)tmp->data);
-            mx_printchar('\n');
-            mx_printstr("Distance: ");
-            mx_printint(distance);
-            mx_printstr("\n========================================\n");
-        }
-        else {
-            mx_printstr((char*)tmp->data);
-            mx_printstr(" -> ");
-        }
-        tmp = tmp->next;
-    }
-}
-
 // void mx_print_path(int start, int end, int distance, t_list* route, t_list* islands) {
 //     printf("========================================\n");
 //     printf("Path: %s -> %s\n", (char*)mx_list_get_by_index(islands, start), (char*)mx_list_get_by_index(islands, end));
